eefcf2d9d560dff21ff8965ffc6d2aff2328cc6b
himaj47 <himajjoshi932@gmail.com>
2025-02-27 21:57:45 +0530

    updated file paths

--- Frontend/JS/p1.js
+++ Frontend/JS/p1.js
@@ -1,5 +1,5 @@
 let sign=document.querySelector('.signup')
 
 sign.addEventListener('click',function(){
-    window.location.href= 'D:/Project2/Frontend/HTML/Login_page.html';
+    window.location.href= '../HTML/Login_page.html';
 });
